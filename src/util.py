af8769cd2a6940a93f3d093beeba3362e8dd323e
sarchakar <sarathbellam95@gmail.com>
2021-05-11 12:27:58 +0530

    add command to get competitions code

--- src/util.py
+++ src/util.py
@@ -4,6 +4,7 @@ data source : https://www.football-data.org/
 """
 import os
 import requests
+import discord
 import json
 from dotenv import load_dotenv
 
@@ -18,14 +19,41 @@ API_ORG = os.getenv("API_ORG")
 
 
 HEADERS = {'X-Auth-Token': API_TOKEN}
+HOME_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_PATH = os.path.join(HOME_DIR, "data")
+
+
+def get_embed(title, description, fields, color=0xf58300):
+    """
+    Method to embed the fields data
+    @param title: Title of the Embed
+    @param description: Description of the embed
+    @param fields: fields to be embed; list of fields where names are-
+                Mandatory : Name, value
+                Optional: binary format(\n\u200b, default is "", inline(True/False; default is True)
+    [{"name": ":one: Laliga", "value": "SPA", "binary_format"=""}, "name": ":one: Bundesliga", "value": "BA", "binary_format"=""}]
+    @param color: font color
+    @return: Embed data
+    """
+    embed = discord.Embed(title=title, description=description, color=color)
+    for field in fields:
+        binary_format = field.get("binary_format", "")
+        inline_ = field.get("inline", True)
+        embed.add_field(name=field["name"], value=field["value"] + binary_format, inline=inline_)
+    return embed
 
 
 def get_data_from_api(url_suffix: str):
     """
     Method to load the data from api
+    @param url_suffix: API suffix (ex : for competitions , url_suffix is competitions
+                       (ref: https://www.football-data.org/)
+    @return: Api response data
     """
+    print(url_suffix)
     url = f"{API_ORG}{url_suffix}"
     response = requests.get(url,headers=HEADERS)
+    print(response)
     json_data = response.json()
     return json_data
 
